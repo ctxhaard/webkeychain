c380f8f8bca09d4e665a73bbcb1b578b2d307529
Carlo Tomasin <c.tomasin@gmail.com>
2021-03-12 13:57:47 +0100

    first commit

---
+++ src/components/AccountField.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mb-3">
+    <label :for="name" class="form-label">{{ label }}</label>
+    <input type="text" class="form-control" :id="name" aria-describedby="`{ name }Help`">
+    <div id="`{ name }Help`" class="form-text">{{ help }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Field',
+  props: {
+    name: String,
+    label: String,
+    help: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AccountShow.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <h1>Account editor</h1>
+    <form action="">
+      <fieldset disabled>
+        <AccountField name="name" label="Account name" help="the account name">{{ account.name }}</AccountField>
+        <AccountField name="url" label="URL" help="the account URL">{{ account.url }}</AccountField>
+        <AccountField name="username" label="Username" help="the account username">{{ account.username }}</AccountField>
+        <AccountField name="password" label="Password" help="the account password">{{ account.password }}</AccountField>
+        <AccountField name="notes" label="Notes" help="the account notes">{{ account.notes }}</AccountField>
+        <div class="mb-3">
+          <button class="btn btn-primary mx-1">Show password</button>
+          <button class="btn btn-primary mx-1">Edit</button>
+          <button type="submit" class="btn btn-primary mx-1">Save</button>  
+        </div>
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script>
+import AccountField from './AccountField.vue'
+export default {
+    components: {
+        AccountField
+    },
+    data() {
+      return {
+        account: {
+          name: 'Google',
+          url: 'http://www.google.com',
+          username: 'mario.rossi@google.com',
+          password: 'acme123',
+          notes: 'these are the account notes'
+      }}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

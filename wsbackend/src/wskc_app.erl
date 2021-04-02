b6f864f981ed0b14eb7bb0441d89fda209fdfeda
Carlo Tomasin <c.tomasin@gmail.com>
2021-04-02 22:47:55 +0200

    cowboy makefile and stub added

--- wsbackend/src/wskc_app.erl
+++ wsbackend/src/wskc_app.erl
@@ -10,7 +10,14 @@
 -export([start/0, start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-  
+  Dispatch = cowboy_router:compile([
+    {'_', [{"/", "base_handler", []}]}
+				   ]),
+  {ok, _} = cowboy:start_clear(
+      my_http_listener, 
+      [{port, 8080}],
+      #{env => #{dispatch => Dispatch}}
+  ).
 
 start() -> kc_app:start([], []).
 

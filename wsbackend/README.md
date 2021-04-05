5b95ed6b0715e65c6beb404da75e7cee4dfcf0ce
Carlo Tomasin <c.tomasin@gmail.com>
2021-04-05 19:49:50 +0200

    websockets implementation stub

---
+++ wsbackend/README.md
@@ -0,0 +1,9 @@
+wskc
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
--- wsbackend/src/wskc_app.erl
+++ wsbackend/src/wskc_app.erl
@@ -7,21 +7,19 @@
 
 -behaviour(application).
 
--export([start/0, start/2, stop/1]).
+-export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-  Dispatch = cowboy_router:compile([
-    {'_', [{"/", "base_handler", []}]}
-				   ]),
-  {ok, _} = cowboy:start_clear(
-      my_http_listener, 
-      [{port, 8080}],
-      #{env => #{dispatch => Dispatch}}
-  ).
-
-start() -> kc_app:start([], []).
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", wskc_base_handler, []}]}
+    ]),
+    {ok, _} = cowboy:start_clear(my_http_listener,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    wskc_sup:start_link().
 
 stop(_State) ->
-  ok.
+    ok.
 
 %% internal functions
